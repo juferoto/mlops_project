5655c2148abacaa6ffce66ddfd0293e4e08aeec3
Juan Felipe Rodriguez <jnferoto@gmail.com>
2024-01-11 22:41:29 -0500

    Cambios a las variables de entorno a exportar

--- training/tests/test_train_model.py
+++ training/tests/test_train_model.py
@@ -6,10 +6,6 @@ from omegaconf import DictConfig
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 """ Aqui se agregan todas las funciones necesarias para realizar pruebas sobre un modelo ML """
-def get_model_version(modelRegisteredVersions: any, stage: str):
-    modelRegistered = next(filter(lambda obj: obj.current_stage == stage, modelRegisteredVersions), None)
-    return modelRegistered.version
-
 def get_model(config: DictConfig):
     # Se agrega la direccion en donde esta alojado MLFlow sea remoto o local
     mlflow.set_tracking_uri(config.mlflow.tracking_ui)
@@ -20,19 +16,6 @@ def get_model(config: DictConfig):
     stage = "Production"
     model_uri=f"models:/{config.model.name}/{stage}"
     model = mlflow.pyfunc.load_model(model_uri)
-
-    client = mlflow.MlflowClient()
-    model_registered = client.get_registered_model(config.model.name)
-    model_version = get_model_version(model_registered.latest_versions, stage)
-    os.environ["MODEL_NAME"] = config.model.name
-    os.environ["MODEL_VERSION"] = model_version
-
-    # Exportar las variables de entorno para GitHub Actions
-    output_line_model = f"{'MODEL_NAME'}={config.model.name}"
-    output_line_version = f"{'MODEL_VERSION'}={model_version}"
-
-    print(f"echo '{output_line_model}' >> $GITHUB_OUTPUT")
-    print(f"echo '{output_line_version}' >> $GITHUB_OUTPUT")
     return model
 
 def load_data(path: DictConfig):
@@ -63,4 +46,4 @@ def test_logistic_regression():
     recall = recall_score(y_test.ravel(), prediction)
 
     # Realizar aserciones sobre los resultados
-    assert accuracy >= 0.7 and precision >= 0.7 and recall >= 0.7
+    assert accuracy >= config.global_metric and precision >= config.global_metric and recall >= config.global_metric
